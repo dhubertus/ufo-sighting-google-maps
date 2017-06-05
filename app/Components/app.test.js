ab22882a673c6e1faca24b34e36fcdbd8a939347
Dave Hubertus <davehubertus@gmail.com>
2017-06-04 22:25:57 -0600

    tested loading screen on app

--- app/Components/app.test.js
+++ app/Components/app.test.js
@@ -28,6 +28,26 @@ describe('Root', () => {
     ReactDOM.render(<Root />, div)
   })
 
+  it('renders a loading page before the api call returns', () => {
+    const wrapper = shallow(<Root />)
+    const loadingState = wrapper.state().loading
+
+    expect(loadingState).toEqual(true)
+
+    const loadingContainer = wrapper.find('#loading-container')
+    expect(loadingContainer.length).toEqual(1)
+  })
+
+  it('has a loading container that holds an img(gif) and loading div that has a two items in it', () => {
+    const wrapper = shallow(<Root />)
+    const loadingContainerImg = wrapper.find('#loading-container').props().children[0].type
+
+    expect(loadingContainerImg).toEqual('img')
+
+    const loadingAnimation = wrapper.find('#loading').props()
+    expect(loadingAnimation.length).toEqual(2)
+  })
+
   it('should return error if it does not fetch initial sightings', () => {
     fetchMock.get('begin:/api/places?randomNumber=' , {
         status: 500,
@@ -54,25 +74,28 @@ describe('Root', () => {
     expect(wrapper.state('viewing')).toEqual({})
   })
 
-  // it('should be able to add a sighting to favorites', async () => {
-  //   fetchMock.get('begin:/api/places?randomNumber=' , {
-  //       status: 200,
-  //     })
-  //   const wrapper = mount(<Root />)
-  //
-  //   await waitingFunc()
-  //   // console.log(wrapper.state());
-  //   const favBtn = wrapper.find('.favorite-btn')
-  //   // console.log(favBtn);
-  // })
-
-  xit('has one HeaderContainer', async () => {
-    // fetchMock.get('begin:/api/places?randomNumber=' , {
-    //   status: 200,
-    // })
+  xit('should be able to add a sighting to favorites', async () => {
+    fetchMock.get('begin:/api/places?randomNumber=' , {
+        status: 200,
+      })
     const wrapper = mount(<Root />)
+
     await waitingFunc()
     // console.log(wrapper.state());
+    const favBtn = wrapper.find('.favorite-btn')
+    // console.log(favBtn);
+  })
+
+  xit('has one HeaderContainer', async () => {
+    const body = initialScrubber(stubbedApiCall)
+
+    fetchMock.get('*' , {
+      status: 200,
+      body: body
+    })
+    const wrapper = mount(<Root />)
+    await waitingFunc()
+    console.log(wrapper.state());
     // console.log(wrapper.nodes[0].props.children);
   })
 
