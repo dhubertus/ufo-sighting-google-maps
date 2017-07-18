051ba6b3e732e6b60561829d8cbad0795eceba7e
Dave Hubertus <davehubertus@gmail.com>
2017-07-17 19:33:54 -0600

    https adjust

--- app/Components/app.js
+++ app/Components/app.js
@@ -81,14 +81,14 @@ import { CardContainer } from './CardContainer/CardContainer'
 
 
 
-    fetch(`http://api.usno.navy.mil/rstt/oneday?date=${month}/${day}/${year}&coords=${latitude},${longitude}&tz=1`)
+    fetch(`https://api.usno.navy.mil/rstt/oneday?date=${month}/${day}/${year}&coords=${latitude},${longitude}&tz=1`)
     .then((res) => res.json())
     .then((obj) => {
       moonPhaseObj = moonScrubber(obj)
     })
 
 
-    fetch(`http://api.wunderground.com/api/be16da1d1979f7aa/history_${year}${month}${day}/q/${state}/${city}.json`)
+    fetch(`https://api.wunderground.com/api/be16da1d1979f7aa/history_${year}${month}${day}/q/${state}/${city}.json`)
     .then((res) => res.json())
     .then((obj) => {
       historicalWeatherObj = weatherScrubber(obj)
