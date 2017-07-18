4ad1462e0ec80f4774d0708cc25e0af5e049226d
Dave Hubertus <davehubertus@gmail.com>
2017-07-17 19:24:15 -0600

    attempt to host

--- src/app.js
+++ src/app.js
@@ -25,12 +25,17 @@ if (process.env.NODE_ENV !== 'production') {
   }));
 }
 
-app.use('/assets', express.static(path.join(__dirname, '../app/assets')));
+app.set('port', process.env.PORT || 3000)
 
-app.get('/', function (req, res) { res.sendFile(path.join(__dirname, '/../index.html')) });
+// app.use('/assets', express.static(path.join(__dirname, '../app/assets')));
+app.use(express.static('app'));
+
+// app.get('/', function (req, res) { res.sendFile(path.join(__dirname, '/../index.html')) });
+app.get('/', function (req, res) { res.sendFile(path.join(__dirname, '../app/index.html')) });
 
 app.use('/api', router);
-app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, '/../index.html')) });
+// app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, '/../index.html')) });
+app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, '../app/index.html')) });
 
 app.listen(port);
 
